c33e81658b8304c61a4488294575fd7459fa796a
calmen <calmenchia@gmail.com>
2021-09-09 23:10:10 +0800

    Add: Response on No button in ConfirmRegistration

--- app/src/main/java/com/calmen/pracgrader/shared/ConfirmRegistration.java
+++ app/src/main/java/com/calmen/pracgrader/shared/ConfirmRegistration.java
@@ -3,6 +3,7 @@ package com.calmen.pracgrader.shared;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.calmen.pracgrader.R;
@@ -18,5 +19,11 @@ public class ConfirmRegistration extends AppCompatActivity {
         Button noRegBtn = findViewById(R.id.noRegBtn);
 
         // TODO: Action for yes and no
+        noRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
