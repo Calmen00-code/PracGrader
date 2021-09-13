16ed83047ede4ea34d3f45d4bb801b20578bde16
calmen <calmenchia@gmail.com>
2021-09-13 20:07:37 +0800

    Add: StudentPracticalList

--- app/src/main/java/com/calmen/pracgrader/models/StudentPracticalList.java
+++ app/src/main/java/com/calmen/pracgrader/models/StudentPracticalList.java
@@ -1,5 +1,21 @@
 package com.calmen.pracgrader.models;
 
+import java.util.ArrayList;
+
 public class StudentPracticalList {
-    private 
+    private ArrayList<Practical> practicals;
+    private int uniqueID;
+
+    public StudentPracticalList(int inUniqueID) {
+        this.practicals = new ArrayList<>();
+        this.uniqueID = inUniqueID;
+    }
+
+    public ArrayList<Practical> getPracticals() {
+        return practicals;
+    }
+
+    public int getUniqueID() {
+        return uniqueID;
+    }
 }
