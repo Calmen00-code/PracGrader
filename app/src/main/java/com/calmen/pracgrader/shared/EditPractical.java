323e8dd6b9e7b2879752decd4ed1b5cc561b46c6
calmen <calmenchia@gmail.com>
2021-09-13 11:47:43 +0800

    Add: EntityQuery for Practical

---
+++ app/src/main/java/com/calmen/pracgrader/shared/EditPractical.java
@@ -0,0 +1,14 @@
+package com.calmen.pracgrader.shared;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditPractical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_practical_page);
+    }
+}
